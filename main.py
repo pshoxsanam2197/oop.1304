2e42fc3a57c50060b1b4d3b5215acea88aa13b3e
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2026-04-13 16:13:53 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,118 @@
+# 1-m
+class Talaba:
+    ism = "Ali"
+    yosh = 18
+    kurs = 1
+
+t1 = Talaba()
+print(f"t1 malumotlari: ")
+print(f"t1 obyektining ismi: {t1.ism}")
+print(f"t1 obyektining ismi: {t1.yosh}")
+print(f"t1 obyektining ismi: {t1.kurs}")
+
+# 2-m
+class Telefon:
+    brend = "Samsung"
+    model = "A15"
+    narx = 200
+
+t1 = Telefon
+print(f"t1 malumotlari: ")
+print(f"t1 obyektining brendi: {t1.brend}")
+print(f"t1 obyektining model: {t1. model}")
+print(f"t1 obyektining narx: {t1.narx}")
+
+# 3-m
+class Kompyuter:
+    protsessor = "i5"
+    rasm = 8
+    ssd = 256
+
+k1 = Kompyuter()
+print(f"k1 malumotlari: ")
+print(f"k1 obyektining protsessori: {k1.protsessor}")
+print(f"k1 obyektining rasm: {k1.rasm}")
+print(f"k1 obyektining ssd: {k1.ssd}")
+
+# 4-m
+class Kitob:
+    nom = "Python"
+    muallif = "Noma'lum"
+    yil = 2024
+k1 = Kitob()
+print(f"k1 malumotlari: ")
+print(f"k1 obyektining nom: {k1.nom}")
+print(f"k1 obyektining muallif: {k1.muallif}")
+print(f"k1 obyektining yil: {k1.yil}")
+
+# 5-m
+class Avtomobil:
+    model = "Cobalt"
+    rang = "Oq"
+    yil = 2023
+
+a1 = Avtomobil()
+print(f"a1 malumotlari: ")
+print(f"a1 obyektining model : {a1.model }")
+print(f"a1 obyektining rang: {a1.rang}")
+print(f"a1 obyektining yil: {a1.yil}")
+
+# 6-m
+class Bank:
+    nom = "Agrobank"
+    valyuta = "UZS"
+    foiz = 20
+
+b1 = Bank()
+print(f"b1 malumotlari: ")
+print(f"b1 obyektining nom : {b1.nom }")
+print(f"b1 obyektining valyuta: {b1.valyuta}")
+print(f"b1 obyektining foiz: {b1.foiz}")
+
+# 7-m
+class Film:
+    nom = "Avatar"
+    janr = "Fantastika"
+    yil = 2022
+
+f1 = Film()
+print(f"f1 malumotlari: ")
+print(f"f1 obyektining nom : {f1.nom }")
+print(f"f1 obyektining janr: {f1.janr}")
+print(f"f1 obyektining yil: {f1.yil}")
+
+# 8-m
+class Oyin:
+    nom = "Mafia"
+    platforma = "PC"
+    reyting = 5
+
+o1 = Oyin()
+print(f"o1 malumotlari: ")
+print(f"o1 obyektining nom : {o1.nom }")
+print(f"o1 obyektining platforma: {o1.platforma}")
+print(f"o1 obyektining reyting: {o1.reyting}")
+
+# 9-m
+class Universitet:
+    nom = "TDYU"
+    davlat = "O‘zbekiston"
+    talabalar_soni = 10000
+
+u1 = Universitet()
+print(f"u1 malumotlari: ")
+print(f"u1 obyektining nomi : {u1.nom }")
+print(f"u1 obyektining davlat: {u1.davlat}")
+print(f"u1 obyektining talabalar_soni: {u1.talabalar_soni}")
+
+# 10-m
+class Restoran:
+    nom = "Osh Markazi"
+    manzil = "Toshkent"
+    reyting = 4
+
+r1 = Restoran()
+print(f"r1 malumotlari: ")
+print(f"r1 obyektining nomi : {r1.nom }")
+print(f"r1 obyektining manzil: {r1.manzil}")
+print(f"r1 obyektining reyting: {r1.reyting}")
